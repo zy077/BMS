b506a68a7111837622b61d8183effbc1eaabb275
zy077 <zy_whynot@163.com>
2020-05-02 12:03:25 +0800

    '创建项目相关目录'

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BMS.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BMS.settings.dev')  # 修改配置文件的路径
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
